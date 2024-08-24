d6c5d649ac92cbf9c0dbd239ab279f5d19bde613
LidiaGodo <lilula739@gmail.com>
2024-08-24 11:42:31 +0300

    add swagger

---
+++ app/Http/Controllers/Swagger/MainController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+class MainController
+{
+
+}
---
+++ app/Http/Controllers/Swagger/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+/**
+ * @OA\PathItem(
+ *     path="/api/"
+ * ),
+ * @OA\Info(
+ *     title="API Docs",
+ *     version="1",
+ * )
+ */
+class MainController
+{
+
+}
