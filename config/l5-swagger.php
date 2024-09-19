4fc6858c1297406fce731b51cf6fb85c01b823b5
LidiaGodo <lilula739@gmail.com>
2024-09-19 14:02:03 +0300

    swagger paths change

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -76,7 +76,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => storage_path('api-docs'),
+            'docs' => storage_path('api-docs/api'),
 
             /*
              * Absolute path to directory where to export views
