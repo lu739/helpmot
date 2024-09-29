9ca42cdcae30aae2085642901a84cd0b8eae662f
LidiaGodo <lilula739@gmail.com>
2024-09-29 14:49:14 +0300

    update enums russian methods

--- app/Enum/OrderStatus.php
+++ app/Enum/OrderStatus.php
@@ -11,4 +11,17 @@ enum OrderStatus: string
     case TIME_EXPIRED = 'time_expired';
     case DRIVER_CANCEL = 'driver_cancel';
     case CLIENT_CANCEL = 'client_cancel';
+
+    public function russian(): string
+    {
+        return match ($this) {
+            self::CREATED => 'Создан',
+            self::ACTIVE => 'Активен',
+            self::IN_PROGRESS => 'В процессе',
+            self::COMPLETED_SUCCESSFULLY => 'Успешно завершен',
+            self::TIME_EXPIRED => 'Время ожидания истекло',
+            self::DRIVER_CANCEL => 'Отменен водителем',
+            self::CLIENT_CANCEL => 'Отменен клиентом',
+        };
+    }
 }
--- app/Enum/UserRole.php
+++ app/Enum/UserRole.php
@@ -7,4 +7,13 @@ enum UserRole: string
     case ADMIN = 'admin';
     case CLIENT = 'client';
     case DRIVER = 'driver';
+
+    public function russian(): string
+    {
+        return match ($this) {
+            self::ADMIN => 'Администратор',
+            self::CLIENT => 'Клиент',
+            self::DRIVER => 'Водитель',
+        };
+    }
 }
